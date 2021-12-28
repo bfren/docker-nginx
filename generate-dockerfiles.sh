e66bdf61e0a20417b9a07bab67f4a8381f984faf
bfren <ben@bcgdesign.com>
2021-12-28 12:03:28 +0000

    v2.5.1 (#64)
    Minor updates
    * Fixing name of executable in output
    * Adding generate-dockerfiles.sh
    * Using latest base images
    * Using bf-test-url in healthcheck
    
    Documentation updates
    * Fixing comments

---
+++ generate-dockerfiles.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker pull bfren/alpine
+
+BASE_REVISION="3.2.2"
+echo "Base: ${BASE_REVISION}"
+
+NGINX_VERSIONS="1.18 1.20 edge"
+for V in ${NGINX_VERSIONS} ; do
+
+    echo "Nginx ${V}"
+    ALPINE_MINOR=`cat ./${V}/ALPINE_MINOR`
+
+    DOCKERFILE=$(docker run \
+        -v ${PWD}:/ws \
+        bfren/alpine esh \
+        "/ws/Dockerfile.esh" \
+        BASE_REVISION=${BASE_REVISION} \
+        ALPINE_MINOR=${ALPINE_MINOR} \
+        NGINX_MINOR=${V}
+    )
+
+    echo "${DOCKERFILE}" > ./${V}/Dockerfile
+
+done
+
+echo "Done."
