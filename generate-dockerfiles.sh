8b142eac3a2c9c74e0095c172073d8c49e019d9a
bfren <ben@bcgdesign.com>
2022-01-23 15:29:41 +0000

    v2.5.2 (#65)
    Minor updates
    * Using latest base images

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="3.2.2"
+BASE_REVISION="3.2.3"
 echo "Base: ${BASE_REVISION}"
 
 NGINX_VERSIONS="1.18 1.20 edge"
@@ -26,4 +26,5 @@ for V in ${NGINX_VERSIONS} ; do
 
 done
 
+docker system prune -f
 echo "Done."
