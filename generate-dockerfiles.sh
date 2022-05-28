0f4f84fa6a96e5c94b4f38292b1c48064cb522df
bfren <ben@bcgdesign.com>
2022-05-28 09:57:36 +0100

    Using latest base images

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="4.2.12"
+BASE_REVISION="4.3.0"
 echo "Base: ${BASE_REVISION}"
 
-NGINX_VERSIONS="1.18 1.20 edge"
+NGINX_VERSIONS="1.18 1.20 1.22 edge"
 for V in ${NGINX_VERSIONS} ; do
 
     echo "Nginx ${V}"
