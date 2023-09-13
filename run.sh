9a168f559cce4c0e6ac7e077ab23b3c94d6f644f
bfren <bf@bfren.dev>
2023-09-13 09:27:23 +0000

    v5.0.14 (#142)
    Minor updates
    * Using latest base images
    
    Development updates
    * Adding --load to run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ IMAGE=`cat VERSION`
 ALPINE=${1:-3.18}
 
 docker buildx build \
+    --load \
     --build-arg BF_IMAGE=nginx \
     --build-arg BF_VERSION=${IMAGE} \
     -f alpine${ALPINE}/Dockerfile \
     -t nginx-alpine${ALPINE}-dev \
     . \
     && \
-    docker run -it nginx-alpine${ALPINE}-dev sh
+    docker run -it -e BF_DEBUG=1 nginx-alpine${ALPINE}-dev sh
