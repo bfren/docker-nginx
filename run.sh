f5c64d2dfb52c3fb7e66eb2669aa3201ec42a466
bfren <bf@bfren.dev>
2024-02-23 18:25:38 +0000

    Removing progress plain

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ ALPINE=${1:-3.19}
 
 docker buildx build \
     --load \
-    --progress plain \
     --build-arg BF_IMAGE=nginx \
     --build-arg BF_VERSION=${IMAGE} \
     -f alpine${ALPINE}/Dockerfile \
