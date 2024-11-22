9f8c604d2b8585e04b3cbb69d6c492afe844e172
bfren <bf@bfren.dev>
2024-11-22 10:20:08 +0000

    Using new /test executable

--- test.sh
+++ test.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-IMAGE=`cat VERSION`
+IMAGE=nginx
+VERSION=`cat VERSION`
 ALPINE=${1:-3.20}
+TAG=${IMAGE}-test
 
 docker buildx build \
     --load \
-    --build-arg BF_IMAGE=nginx \
-    --build-arg BF_VERSION=${IMAGE} \
+    --build-arg BF_IMAGE=${IMAGE} \
+    --build-arg BF_VERSION=${VERSION} \
     -f alpine${ALPINE}/Dockerfile \
-    -t nginx-alpine${ALPINE}-test \
+    -t ${TAG} \
     . \
     && \
-    docker run --rm nginx-alpine${ALPINE}-test env -i nu -c "use bf test ; test"
+    docker run --entrypoint /test ${TAG}
